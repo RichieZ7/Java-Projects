67c034fe4c511c552c3c001815c791c01f01c3b9
RichieZ7 <66334204+RichieZ7@users.noreply.github.com>
2020-06-03 22:38:35 -0400

    Update BullyCalculator.java

--- BullyCalculator.java
+++ BullyCalculator.java
@@ -1,4 +1,4 @@
-//This program is a modification of the original GPA calculator program
+//This program is a modification of the original GPA calculator program V.2.0
 import java.util.Scanner;
 
 public class BullyCalculator {
@@ -11,24 +11,63 @@ public class BullyCalculator {
 	double sumCalculation;
 	double totalCredits;
 	double GPA;
+	double defaultWeight;
+	double actualWeight;
+	boolean isValid;
+	int answer1;
+	double answer2;
 	
 	public BullyCalculator() {
 		
 	}
 	
+	public void validateInput(String startingQuestion, String errorMessage, String errorMessageTwo, String validDataType, double lowerBound, double upperBound) {
+		do {
+			System.out.print(startingQuestion + "\n> ");
+			if (validDataType.equals("int")) {
+				if (keyboard.hasNextInt()) {
+					answer1 = keyboard.nextInt();
+					isValid = true;
+					if (!(answer1 >= lowerBound && answer1 <= upperBound)) {
+						System.out.print(errorMessage + "\n");
+						isValid = false;
+					}
+				}
+				else {
+					System.out.println(errorMessageTwo);
+					isValid = false;
+					keyboard.next();
+				}
+			}
+			else if (validDataType.equals("double")) {
+				if (keyboard.hasNextDouble()) {
+					answer2 = keyboard.nextDouble();
+					isValid = true;
+					if (!(answer2 >= lowerBound && answer2 <= upperBound)) {
+						System.out.print(errorMessage + "\n");
+						isValid = false;
+					}
+				}
+				else {
+					System.out.println(errorMessageTwo);
+					isValid = false;
+					keyboard.next();
+				}
+			}
+		} while (!isValid);
+	}
+	
 	public void calculateGPA() {
-		System.out.print("How many classes do you take?\n> ");
-		numberOfClasses = keyboard.nextInt();
+		validateInput("How many classes are you taking?", "Don't try to outsmart me, that can't be right.", "Invalid input. Put in a number.", "int", 5 , 8 );
+		numberOfClasses = answer1;
 		for (int i = 1; i <= numberOfClasses; i++) {
 			System.out.println("Class " + i + ":");
-			System.out.print("How many credits is this class? ");
-			creditsForClass = keyboard.nextDouble();
-			System.out.println("What is the course difficulty?");
-			System.out.print("Options: \n1. Academic \n2. Honors \n3. AP \n> ");
-			courseType = keyboard.nextInt();
-			System.out.println("What was your grade in this class?");
-			System.out.print("Options: \n1. A+ \n2. A \n3. A- \n4. B+ \n5. B \n6. B- \n7. C+ \n8. C \n9. C- \n10. D+ \n11. D \n12. D- \n13. U \n> ");
-			gradeInClass = keyboard.nextInt();
+			validateInput("How many credits is this class?", "Don't try to trick me. Type in how many credits the class is really worth.", "Invalid input. Put in a number.", "double", 2.5 , 7 );
+			creditsForClass = answer2;
+			validateInput("What is the course difficulty?\nOptions: \n1. Academic \n2. Honors \n3. AP ", "I have a suspicion that this kind of class does not exit.", "Invalid input. Put in a number.", "int", 1, 3);
+			courseType = answer1;
+			validateInput("what was your grade in this class?\nOptions: \n1. A+ \n2. A \n3. A- \n4. B+ \n5. B \n6. B- \n7. C+ \n8. C \n9. C- \n10. D+ \n11. D \n12. D- \n13. U ","I'm sure you know how to type in numbers. Type in a valid number.","Invalid input. Put in a number.", "int", 1, 13 );
+			gradeInClass = answer1;
 			if (gradeInClass == 1) {
 				System.out.println("You are average.");
 			}
@@ -37,8 +76,7 @@ public class BullyCalculator {
 				System.out.println("Go study instead.");
 				System.exit(0);
 			}
-			double defaultWeight = 4.33333;
-			double actualWeight;
+			defaultWeight = 4.33333;
 			if (courseType == 2) {
 				defaultWeight = defaultWeight + 0.66667;
 			}
